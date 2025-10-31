b98ad6f8e4ebddd91c9c99dfb83e740f6f1bdb86
Dinesh K <dinesh.k@admindroid.dev>
2025-10-31 10:15:04 +0530

    output text box added to custom pop up

--- OfficeMaster/UI/CustomPopup.cs
+++ OfficeMaster/UI/CustomPopup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System;
 using System.Windows.Forms;
-using static OfficeHelper.TimeAggregator;
 
 namespace OfficeHelper
 {
@@ -9,45 +8,55 @@ namespace OfficeHelper
     {
         public int SelectedOption { get; private set; }
 
+        private static RichTextBox resultBox;
+
         public CustomPopup()
         {
-            this.Text = "Custom Popup";
-            this.Width = 250;
-            this.Height = 300;
+            this.Text = "Time Tracker";
+            this.Width = 300;
+            this.Height = 350;
+
+            resultBox = new RichTextBox
+            {
+                Left = 20,
+                Top = 200,
+                Width = 250,
+                Height = 100,
+                ReadOnly = true,
+                BackColor = System.Drawing.Color.WhiteSmoke,
+                BorderStyle = BorderStyle.FixedSingle
+            };
 
             var button1 = new Button { Text = "Work Start", Left = 20, Top = 50, Width = 80 };
             var button2 = new Button { Text = "Work End", Left = 110, Top = 50, Width = 80 };
             var button3 = new Button { Text = "Break Start", Left = 20, Top = 100, Width = 80 };
             var button4 = new Button { Text = "Break End", Left = 110, Top = 100, Width = 80 };
-            var button5 = new Button { Text = "Monthly TotalHours", Left = 110, Top = 100, Width = 80 };
-            //var button6 = new Button { Text = "Monthly OfficeHours", Left = 110, Top = 100, Width = 80 };
-            //var button7 = new Button { Text = "Monthly BreakHours", Left = 110, Top = 100, Width = 80 };
-            //var button8 = new Button { Text = "Monthly CompensatedHours", Left = 110, Top = 100, Width = 80 };
-
-
-
+            var button5 = new Button { Text = "Monthly TotalHours", Left = 20, Top = 150, Width = 80 };
 
             button1.Click += (s, e) => { SelectedOption = (int)TimeAggregator.Events.WorkStart; this.DialogResult = DialogResult.OK; };
             button2.Click += (s, e) => { SelectedOption = (int)TimeAggregator.Events.WorkEnd; this.DialogResult = DialogResult.OK; };
             button3.Click += (s, e) => { SelectedOption = (int)TimeAggregator.Events.BreakStart; this.DialogResult = DialogResult.OK; };
             button4.Click += (s, e) => { SelectedOption = (int)TimeAggregator.Events.BreakEnd; this.DialogResult = DialogResult.OK; };
-            button5.Click += (s, e) => { SelectedOption = (int)TimeAggregator.Events.CalculatedHours; this.DialogResult = DialogResult.OK; };
-            //button6.Click += (s, e) => { SelectedOption = (int)TimeAggregator.MontlyAggregate.OfficeHours; this.DialogResult = DialogResult.OK; };
-            //button7.Click += (s, e) => { SelectedOption = (int)TimeAggregator.MontlyAggregate.BreakHours; this.DialogResult = DialogResult.OK; };
-            //button8.Click += (s, e) => { SelectedOption = (int)TimeAggregator.MontlyAggregate.CompensateHours; this.DialogResult = DialogResult.OK; };
-
-
-
+            button5.Click += (s, e) => { ShowResult(); };
 
             this.Controls.Add(button1);
             this.Controls.Add(button2);
             this.Controls.Add(button3);
             this.Controls.Add(button4);
             this.Controls.Add(button5);
-            //this.Controls.Add(button6);
-            //this.Controls.Add(button7);
-            //this.Controls.Add(button8);
+            this.Controls.Add(resultBox);
 
         }
+
+        public static void ShowResult(string message = null)
+        {
+            if(message != null)
+            {
+                resultBox.Text = message;
+                return;
+            }
+            resultBox.Text = EventHandler.FetchAggregatedData();
+        }
+
     }
 }
